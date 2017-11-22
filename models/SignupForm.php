f0f7e661852b711a5a9bb25f164f2dbe14d03a5b
Wasth <riasta@yandex.ru>
2017-11-22 14:33:03 +0300

    Starting work with MyServices

---
+++ models/SignupForm.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Yula
+ * Date: 22.11.2017
+ * Time: 13:56
+ */
+
+namespace app\models;
+
+
+class SignupForm
+{
+
+}
---
+++ views/site/myservices.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Yula
+ * Date: 22.11.2017
+ * Time: 14:27
+ */
